c28eead76e1b5d1c7aa22b641f83223aa41e1a24
lelalewis <lela.lewis@ascte.org>
2025-04-22 11:22:28 -0500

    Added post feature (have to refresh)

--- static/js/app.js
+++ static/js/app.js
@@ -7,11 +7,23 @@ function renderPost(post) {
     document.getElementById("feed").appendChild(template);
 }
 
-function submitPost() {
+async function submitPost() {
     const message = document.getElementById("postInput").value;
-    console.log("Would post:", message);
-    alert("Tweet submitted (not really yet)");
-}
+    try{
+        const response = await fetch("/api/add_posts",{
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json",
+        }, 
+        body:JSON.stringify({
+            username,
+            message,
+        }),
+    });
+
+    } catch (error) {
+        console.log("😭 Post failed", error)
+    }
 
 window.onload = async () => {
     try {
@@ -19,6 +31,7 @@ window.onload = async () => {
         const posts = await response.json();
         posts.forEach((post) => renderPost(post));
     } catch (error){
-      console.error("Error fetching posts:", error)  
+      console.error("Error fetching posts:", error) 
     }
-};
+}}
+ 
