1c0dcbcc43748abdd62f32faf840cab7e63584bf
lelalewis <lela.lewis@ascte.org>
2025-05-02 10:00:29 -0500

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -43,4 +43,15 @@ function renderPost(post, isNew = false) {
       console.error("Error fetching posts:", error);
     }
   };
+
+  setInterval(async () => {
+    try {
+      const response = await fetch("/api/posts");
+      const posts = await response.json();
+      posts.forEach((post) => renderPost(post));
+    } catch (error) {
+      console.error("Error fetching posts:", error);
+    }
+
+  }, 5000);
   
